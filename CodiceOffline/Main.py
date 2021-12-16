84564542a34727600d439f7171d12392095a8363
Piter996 <79835822+Piter996@users.noreply.github.com>
2021-12-16 15:22:45 +0100

    Location File updated

--- CodiceOffline/Main.py
+++ CodiceOffline/Main.py
@@ -1,11 +1,8 @@
 import numpy
+import pandas as pd
+
 from LinkCam import *
 from os import *
-from io import *
-
-
-
-import pandas as pd
 
 PATH_CSV =os.getcwd() + '\\res\\CamLocations\\Cam.csv'
 
@@ -13,23 +10,22 @@ PATH_CSV = 'C:\\Users\\P1T3R\\Documents\\GitHub\\ICon\\CodiceOffline\\res\\CamLo
 
 #print (PATH_CSV)
 
-Column_X = 'X'
-
-
+#estrae il DataFRame riguardante la colonna richiesta
 data_X = pd.read_csv(PATH_CSV, usecols = ['X'])
 data_Y = pd.read_csv(PATH_CSV, usecols = ['Y'])
 data_ID = pd.read_csv(PATH_CSV, usecols = ['ID'])
 
-
+#Inizializza la classe Sorveglianza che conterra le info riguardanti le telecamere
 Controller = Sorveglianza
 Controller._init_(Controller)
 
-
+#Conversione dei DataFrame in array per facilitare il caricamneto
 import_X = data_X.to_numpy()
 import_Y = data_Y.to_numpy()
 import_ID = data_ID.to_numpy()
 
 
+#Ciclo di caricamento dei dati nella classe Sorveglianza
 for i,h,j in zip(import_X,import_Y,import_ID):
     
     Controller.update_X(Controller,i)
